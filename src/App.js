010160b26a6d558b42da7d172afbf626dfc5f5f4
Razan-2001 <Razan741422@gmail.com>
2024-03-24 23:31:47 +0300

    project1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Login from './Components/Login/Login';
+import './App.css';
+
+function App() {
+  return (  
+    <div>
+  <Login/>
+    
+    </div>
+
+
+  );
+}
+
+export default App;
